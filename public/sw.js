f2e0db17f36d7ca5a7719882822efe2fbec94745
Jagzgotspark <jagritisingh1285@gmail.com>
2026-06-04 23:14:25 +0530

    feat: PWA manifest and service worker

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'city-pulse-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/static/js/main.chunk.js', '/static/css/main.chunk.css'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS).catch(() => {}))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/api/')) return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
