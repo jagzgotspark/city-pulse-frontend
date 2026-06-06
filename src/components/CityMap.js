07109431b26999661173667fe5d0ae5bc57ef153
Jagzgotspark <jagritisingh1285@gmail.com>
2026-06-06 23:24:47 +0530

    fix: install leaflet.heat as npm package

--- src/components/CityMap.js
+++ src/components/CityMap.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 import { MapContainer, TileLayer, CircleMarker, Popup, Tooltip } from 'react-leaflet';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
+import 'leaflet.heat';
 
 function getPulseColor(score) {
   if (score >= 70) return '#22c55e';
